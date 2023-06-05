f01350df8ebcd742d93c7ddd1b26fd41645ed7d1
trey_weathers <treychelle.weathers2017@gmail.com>
2023-06-05 14:08:50 -0400

    site cleanup

--- portfolio_site/website/src/components/contact.js
+++ portfolio_site/website/src/components/contact.js
@@ -1,8 +1,17 @@
 export default function Contact() {
     return (
         <div>
-          <h4>email: techteachertrey@gmail.com</h4>
-          <h4>LinkedIn: https://www.linkedin.com/in/teachertrey/</h4>
+          <ul>
+
+            <li>
+          <a className="msglink" href="https://www.linkedin.com/in/teachertrey/"> LinkedIn </a>
+          </li>
+
+          <li>
+          <a className="msglink" href="https://docs.google.com/forms/d/e/1FAIpQLSfx8rrHRvZVKwNzhOCqloYEAXB0cLLrBxR9DBzJG3VFVsp2Fg/viewform?usp=sf_link"> Send me a message! </a>
+          </li>
+
+          </ul>
         </div>
       )
     }
