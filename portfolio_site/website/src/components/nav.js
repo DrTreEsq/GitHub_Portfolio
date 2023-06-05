e30a8b3ab5f81213544298b0f50c97dcc0e93a27
trey_weathers <treychelle.weathers2017@gmail.com>
2023-06-05 11:50:36 -0400

    Update css app cleanup with content

--- portfolio_site/website/src/components/nav.js
+++ portfolio_site/website/src/components/nav.js
@@ -6,8 +6,8 @@ export default function Nav({Home, Aboutme , Contact, Services}) {
       <nav className="homenav">
           <Link className="link" to={Home}> HOME </Link>
           <Link className="link" to={Aboutme}> ABOUT ME </Link>
-          <Link className="link" to={Contact}> CONTACT </Link>
           <Link className="link" to={Services}> SERVICES </Link>
+          <Link className="link" to={Contact}> CONTACT </Link>
       </nav>
     </div>
   );
