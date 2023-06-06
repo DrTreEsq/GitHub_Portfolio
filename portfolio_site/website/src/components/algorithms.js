42c841fdf10bc29f00336735b7094012b6e4b5fc
trey_weathers <treychelle.weathers2017@gmail.com>
2023-06-06 13:49:30 -0400

    algorithms page cleanup

--- portfolio_site/website/src/components/algorithms.js
+++ portfolio_site/website/src/components/algorithms.js
@@ -23,6 +23,8 @@ export default function Algorithms() {
         </div>
         <hr></hr>
 
+        <a className="github" href="https://github.com/TREYCSE/GitHub_Portfolio">GitHub PortFolio README.md for Engineers: Click here for code samples, etc.</a>
+
         <h2> Example 1: Linear Regression Example - Market Analysis for Profit Prediction </h2>
         
         <div className="example1 black">
@@ -64,17 +66,10 @@ export default function Algorithms() {
           
           <h3>Tools: Python, TensorFlow, tensorflow.keras, matplotlib</h3>
           <h3>Results</h3>
+          <img className="example" src="https://i.postimg.cc/WbgvfPc7/Image-6-6-23-at-1-42-PM.jpg" align="center" alt="vector"></img>
 
         </div>
           <h3>Visualizations</h3>
-          <img className="example" src="https://i.postimg.cc/Fznyb7wT/C2-W1-Vector-Matrix.png" align="center" alt="vector"></img>
-          <img className="example" src="https://i.postimg.cc/L6nB93Zw/C2-W1-Roasting-Network.png" align="center" alt="roasting"></img>
-          <img className="example" src="https://i.postimg.cc/hj2TgX4K/C2-W1-Roasting-Decision.png" align="center" alt="decision"></img>
-          <img className="example" src="https://i.postimg.cc/wxbLGRbV/C2-W1-Neurons-And-Layers.png" align="center" alt="neurons"></img>
-          <img className="example" src="https://i.postimg.cc/8PJMQk2V/C2-W1-Matrix-Matrix.png" align="center" alt="matrix"></img>
-          <img className="example" src="https://i.postimg.cc/xTMGNTt3/C2-W1-L3-Lab01-3-Neurons.png" align="center" alt="neurons"></img>
-          <img className="example" src="https://i.postimg.cc/fbL0CwjF/C2-W1-dense2.png" align="center" alt="dense"></img>
-          <img className="example" src="https://i.postimg.cc/x1Fd0LMx/C2-W1-dense.png" align="center" alt="dense1"></img>
           
           <hr></hr>
         </div>
