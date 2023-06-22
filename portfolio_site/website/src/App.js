b690da71118778f7cd92c6764cf06960d1d6158b
trey_weathers <treychelle.weathers2017@gmail.com>
2023-06-21 23:26:30 -0400

    Cannot POST / error - create route path / url for post

--- portfolio_site/website/src/App.js
+++ portfolio_site/website/src/App.js
@@ -2,10 +2,10 @@ import * as React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Nav, Home, Aboutme, Algorithms, Codes, Consulting, Contact, Education, Skills, Services, Tutorials, YoutubeEmbed, YoutubeEmbed2, YoutubeEmbed3} from './components/index.js'
 import './App.css';
-import axios from "axios";
+//import axios from "axios";
 
 function App() {
-  Api();
+  //Api();
 
   return (
     <div className="App">
@@ -25,24 +25,33 @@ function App() {
         <Route path="/hardware" element={<YoutubeEmbed embedId="Utwnm2kjYAM" embedId3="4SA6t_tcFQA"/>} />
         <Route path="/neural_networks" element={<YoutubeEmbed2 embedId2="_c_x8A3mNDk"/>} />
         <Route path="/production" element={<YoutubeEmbed3 embedId="NgWujOrCZFo"/>} />
+        <Route path="/post" />
       </Routes>
 
-      <form className="form">
-        <button onClick={Api}>Connected??</button>
+
+      <form action="../../" method="post" className="form">
+      <button type="submit">Connected?</button>
       </form>
+
 
     </div>
   );
 }
 
+
+/* backup api - different error, use full API axios syntax OR debug that ^^ - 
+//cannot POST - 3000/post - is that where that is supposed to be going
+//whatever is after the last / is what URL it goes to - 3000/... just not sure what my target is
+//CREATE ROUTE FOR POST PATH - WITH REACT ROUTER?
+
+<form className="form">
+<button onClick={Api}>Connected??</button>
+</form>
+
 function Api () {
   return axios.post("https://http://localhost:8080/")
 }
 
-/*
-      <form action="../../post" method="post" className="form">
-      <button type="submit">Connected?</button>
-      </form>
 */
 
 export default App;
