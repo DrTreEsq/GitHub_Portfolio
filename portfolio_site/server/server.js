ec32a3288cda79f80c21704f7f6d6c56ccedba5d
trey_weathers <treychelle.weathers2017@gmail.com>
2023-06-24 23:04:58 -0400

    Update server.js

--- portfolio_site/server/server.js
+++ portfolio_site/server/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) =>{
-  res.send("Hello World!");
-})
+const hostname = "127.0.0.1";
 
 const PORT = process.env.PORT || 8080;
-  
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
-  
+app.get("/", (req, res) =>{
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World\n");
+})
+
+app.listen(PORT, hostname, () => {
+  console.log(`Server started on port ${hostname}:${PORT}/`)
+});
