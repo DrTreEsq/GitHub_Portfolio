f95cef0aefbb63709d4730548b3c3b289d20be2d
trey_weathers <treychelle.weathers2017@gmail.com>
2023-06-29 09:05:31 -0400

    Update server.js

--- portfolio_site/website/src/server/server.js
+++ portfolio_site/website/src/server/server.js
@@ -2,16 +2,8 @@ const express = require("express");
 const app = express();
 
 const hostname = "127.0.0.1";
-
 const PORT = process.env.PORT || 8080;
 //stack overflow says use WEB_APP configuration instead of env.PORT for production
-
-
-//update on Azure
-//1. azure not redeploying saying "not found" - don't know if recent changes have been taking into effect
-//2. looking into docker log files on computer  - debugging new error message for wwwroot looking for server.js,
-//^^ tried putting in src folder for react site, not sure if those changes have hit yet bc not redeploying
-//try looking at ways to view in browser so see how running in Azure
 
 app.get("/", (req, res) =>{
   console.log(res.statusCode = 200);
@@ -23,15 +15,18 @@ app.listen(PORT, hostname, () => {
   console.log(`Server started on port ${hostname}:${PORT}/`)
 });
 
-
-//https://emswebsite.scm.azurewebsites.net/api/logs/docker/zip
-
+//update on Azure TroubleShooting before Error Handling Scripts
+//1. azure not redeploying saying "not found" - don't know if recent changes have been taking into effect
+//2. using docker logs recommended in Azure troubleshooting: https://emswebsite.scm.azurewebsites.net/api/logs/docker/zip
+//looking into docker log files on computer  - debugging new error message
+//index for wwwroot looking for server.js, 
+//3. viewing react in browser: docker, azure, local host, etc.
+//4.stack overflow says use WEB_APP configuration instead of env.PORT for production
+//^^ tried putting in src folder for react site, not sure if those changes have hit yet bc not redeploying
 
 /* http instead of express alternative syntax
 const http = require("http");
-
 const hostname = "127.0.0.1";
-
 const PORT = process.env.PORT || 8080;
 
 const server = http.createServer ((req, res) =>{
