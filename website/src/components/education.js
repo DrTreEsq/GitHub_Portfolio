da49a38ef45d44ac70008ea1b36bab9ccadcdce3
trey_weathers <treychelle.weathers2017@gmail.com>
2023-07-03 11:25:24 -0400

    Update education.js

--- website/src/components/education.js
+++ website/src/components/education.js
@@ -7,12 +7,18 @@ export default function Education() {
 
           <img className="example" src="https://i.postimg.cc/3Jrk9k3k/Image-6-20-23-at-7-27-AM.jpg" align="center" alt="vector"></img>
 
-          <h3 className="blue"> Stanford Machine Learning Specialization Certificate | General Assembly Software Engineering Certificate | Pursuing Graduate Education in Artificial Intelligence</h3>
-          <h4> ✡ B.S. in Healthcare Management & attended law school for one year before being consumed by my passion for S.T.E.M.</h4>
-          <h4> ✡ General Assembly: Full Stack Software Engineering, May 2023</h4>
-          <h4> ✡ Stanford & DeepLearning.AI: Machine Learning Specialization Certificate, June 2023</h4>
+          <h3 className="blue"> Completed Education </h3>
+          <h4> ✡ B.S. in Healthcare Management & attended Fordham Law School for one year before being consumed by my passion for S.T.E.M.</h4>
+          <h4> ✡ General Assembly: Full Stack Software Engineering </h4>
+          <h4> ✡ Stanford & DeepLearning.AI: Machine Learning Specialization Certificate </h4>
 
           <hr className="hrh"></hr>
+
+          <h3 className="blue"> Currently Enrolled in </h3>
+          <h4> ✡ IBM: Information Technology (IT) and Cloud Fundamentals Specialization Certificate</h4>
+          <h4> ✡ DeepLearning.AI: Mathematics for Machine Learning and Data Science Specialization Certificate</h4>
+          <h5> ❂ Working Towards MS in Computer Science next Fall ❂ </h5>
+          
 
           <h3 className="blue">Positions open to</h3>
             <h5>✰ Fields of Interest and Experience: Artificial Intelligence, Medical, Business & Media, Robotics, Web Development, Law/Policy, Education, Data Science, and more! </h5>
