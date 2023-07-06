29652c0c7a26c8ffb6b2700ac14bbe92878ab6ad
trey_weathers <treychelle.weathers2017@gmail.com>
2023-07-05 20:26:03 -0400

    Update Virtual Classroom repo link etc.

--- website/src/components/codes.js
+++ website/src/components/codes.js
@@ -3,18 +3,19 @@ export default function Codes() {
     <div className="">
 
 <hr></hr>
-        <a className="github" href="https://github.com/TREYCSE/Virtual_Detention_TensorFlowcpu"> GitHub Link - click here to go to developer repo for this project! </a>
         <h2 className=""> Example 1: This Front End Website! </h2>
-        <p className="black">If it was not already assumed, I wrote the code for this website entirely myself, using specically a React.js frontend framework</p>
+        <p className="black">If it was not already assumed, I wrote the code for this website entirely myself, using a React.js frontend and correlating frameworks, and
+        used GitHub pages, yarn, and Microsoft Azure to deploy </p>
 
         <h2 className=""> Example 2: Full Stack Website The Virtual Detention with Virtual Tutor</h2>
 
         <p className="black">
           What is this "Virtual Detention"? The Virtual Detention is an online classroom intended for students that tend to get bad reps in tradidional classrooms.
             Using Natural Language Question Answering BERT models trained with SQuAD, along with React and Node on the front end,
-            students can interact with this platform to help study outside of normal school environments or hours.</p>
+            students can interact with this platform to help study outside of normal school environments or hours
+            The Backend database is built with mongodb and it is in development now! </p>
 
-
+            <a className="github" href="https://github.com/TREYCSE/Virtual_Classroom/tree/main"> GitHub Link - click here to go to developer repo for this project! </a>
         <h3>ToolKit</h3>
 
         <p className="black">TensorFlow BERT Model from TensorFlowHub w/ Tensorflow cpu, WebGL, Node.js, React.js & Heroku to deploy
