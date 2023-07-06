ef2d450121e458910f19d979c5094a1c1c615758
trey_weathers <treychelle.weathers2017@gmail.com>
2023-07-06 09:21:44 -0400

    Update codes.js

--- website/src/components/codes.js
+++ website/src/components/codes.js
@@ -4,23 +4,21 @@ export default function Codes() {
 
 <hr></hr>
         <h2 className=""> Example 1: This Front End Website! </h2>
-        <p className="black">If it was not already assumed, I wrote the code for this website entirely myself, using a React.js frontend and correlating frameworks, and
-        used GitHub pages, yarn, and Microsoft Azure to deploy </p>
+        <p className="black">If it was not already assumed, I wrote the code for this website entirely myself, using a React.js frontend and correlating frameworks.
+        I used GitHub actions, yarn, and Microsoft Azure to deploy, and for data seeding I am working with MongoDB.</p>
 
         <h2 className=""> Example 2: Full Stack Website The Virtual Detention with Virtual Tutor</h2>
 
         <p className="black">
-          What is this "Virtual Detention"? The Virtual Detention is an online classroom intended for students that tend to get bad reps in tradidional classrooms.
-            Using Natural Language Question Answering BERT models trained with SQuAD, along with React and Node on the front end,
-            students can interact with this platform to help study outside of normal school environments or hours
-            The Backend database is built with mongodb and it is in development now! </p>
+        Simply put, the Virtual Classroom / Detention is where the non-traditional students like me get sent to go supplement our clasroom education after being kicked out
+        of traditional classrooms for "poor classroom performance" so they can learn on their own time! </p>
 
-            <a className="github" href="https://github.com/TREYCSE/Virtual_Classroom/tree/main"> GitHub Link - click here to go to developer repo for this project! </a>
         <h3>ToolKit</h3>
-
-        <p className="black">TensorFlow BERT Model from TensorFlowHub w/ Tensorflow cpu, WebGL, Node.js, React.js & Heroku to deploy
+        <p className="black">TensorFlow BERT Model from TensorFlowHub w/ Tensorflow cpu, WebGL, Node.js, React.js & Azure to deploy
           TensorFlow models are BERT Q&A models for Natural Language Question Answering: Answer questions based off content of a given passage,
-          serving as a virtual tutor in The Virtual Detention</p>
+          serving as a virtual tutor in The Virtual Classroom Detention</p>
+        <a className="github" href="https://github.com/TREYCSE/Virtual_Classroom/tree/main"> GitHub Link - click here to go to developer repo for this project | </a>
+        <a className="github" href="https://github.com/TREYCSE/Virtual_Classroom/tree/main"> Site Link - click here to to the Virtual Class! </a>
 
        <h3>Snapshots</h3>
        <img className="exampleer" src="https://i.imgur.com/9qRZRqA.jpg" align="center" alt="capstone1"></img>
