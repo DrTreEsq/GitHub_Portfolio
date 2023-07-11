4e18c60deb3c94489ed6712afcef1372a9c89a1f
The Ethical Mad Scientist <117105883+TREYCSE@users.noreply.github.com>
2023-07-11 10:21:33 -0400

    Update codes.js

--- website/src/components/codes.js
+++ website/src/components/codes.js
@@ -7,7 +7,7 @@ export default function Codes() {
         <p className="black">If it was not already assumed, I wrote the code for this website entirely myself, using a React.js frontend and correlating frameworks.
         I used GitHub actions, yarn, and Microsoft Azure to deploy, and for data seeding I am working with MongoDB.</p>
 
-        <h2 className=""> The Virtual Classroom </h2>
+        <h2 className=""> Example 2: The Virtual Classroom </h2>
 
         <p className="black">
         Simply put, the Virtual Classroom / Detention is where the non-traditional students like me get sent to go supplement our clasroom education after being kicked out
@@ -19,7 +19,7 @@ export default function Codes() {
           TensorFlow models are BERT Q&A models for Natural Language Question Answering: Answer questions based off content of a given passage,
           serving as a virtual tutor in The Virtual Classroom Detention</p>
         <a className="github" href="https://proud-plant-0981ce810.3.azurestaticapps.net/"> Site Link - click here to to the Virtual Class! </a>
-
+<hr>
         <img className="exampleer" src="https://i.postimg.cc/vmQfCVr6/0954-CFAF-BFE4-42-A3-BDDF-84-C940473277-1-201-a.jpg" align="center" alt="capstone1"></img>
 
        <h3>Snapshots</h3>
