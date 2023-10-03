200b0ce127d386386a46af8635e444be018407e6
The Ethical Mad Scientist <117105883+TREYCSE@users.noreply.github.com>
2023-10-03 14:21:50 -0400

    Update data.js

--- website/src/components/data.js
+++ website/src/components/data.js
@@ -1,3 +1,11 @@
+export default function Data() {
+  return (
+    <div className="">
 
+          <h3> Web scraping tool and databases in progress as well as 2 Google  Data professional certificates! </h3>
+       
+      </div>
+    )
+  }
 
 
