22a370268bd7d180e67bcdf24982646430dc042d
The Ethical Mad Scientist <117105883+TREYCSE@users.noreply.github.com>
2023-10-03 14:19:32 -0400

    Update services.js

--- website/src/components/services.js
+++ website/src/components/services.js
@@ -5,7 +5,7 @@ export default function Services() {
       <div className="">
         
         <h5> click for code samples, results, summaries, and visuals from my completed and in progress projects! </h5>
-        <Navd Algorithms="/algorithms"  Codes="/codes" Consulting="/consulting" Data="/data"/>
+        <Navd Data="/data" Algorithms="/algorithms"  Codes="/codes" Consulting="/consulting" />
         <hr></hr>
         <a className="github" href="https://github.com/TREYCSE/GitHub_Portfolio">GitHub PortFolio README.md for Engineers: Click here for code samples, etc.</a>
         
