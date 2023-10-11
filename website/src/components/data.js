94514d5207d718943023fadb7ac4295a81da94c2
The Ethical Mad Scientist <117105883+TREYCSE@users.noreply.github.com>
2023-10-11 10:21:29 -0400

    Update data.js

--- website/src/components/data.js
+++ website/src/components/data.js
@@ -2,10 +2,12 @@ export default function Data() {
   return (
     <div className="">
 
-          <a href = "https://github.com/TREYCSE/Web_Scraper_csv">  Web scraping tool in progress </a>
-          <hr> </hr>
+          <a href = "https://github.com/TREYCSE/Web_Scraper_csv" className="black">  Web scraping tool in progress </a>
           <h4> The Web Scraper is made with Scrapy, xpath, and the os module in Python. I am scraping data from the NASA government website as physics is one of my special interests. </h4>
-          <a href = "https://github.com/TREYCSE/Data_SQL"> Database in progress </a>
+          
+    <hr>
+    
+          <a href = "https://github.com/TREYCSE/Data_SQL" className="black"> Database in progress </a>
           <h4> In progress... </h4>
       </div>
     )
