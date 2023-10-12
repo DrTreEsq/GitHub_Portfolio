7e674830c014f7f81dcd0e5a73de1b31a935875d
The Ethical Mad Scientist <117105883+TREYCSE@users.noreply.github.com>
2023-10-12 05:51:46 -0400

    Update home.js

--- website/src/components/home.js
+++ website/src/components/home.js
@@ -5,7 +5,7 @@ export default function Home() {
         <h1 className="title white"> Trey Weathers</h1>
         <img className="meimg" src="https://i.postimg.cc/9QnRRJJ3/IMG-6355.jpg" alt="that's me right there!"></img>
         <h4>‼️Please note: This website is under construction, use caution when naviagting - press the tab for the page you wish to reach, instead of pressing back will get 404 error.
-              Routing is being fixed later. Thanks for your understanding! The EMS has many important projects emerging soon, stay posted!
+              Routing is being fixed later. Thanks for your understanding! The EMS has many important projects emerging soon, stay posted! </h4>
       </div>
     )
   }
