3459877d2ae9855537416f655221604c64b5620f
The Ethical Mad Scientist <117105883+TREYCSE@users.noreply.github.com>
2023-10-12 05:47:44 -0400

    Update data.js

--- website/src/components/data.js
+++ website/src/components/data.js
@@ -6,7 +6,7 @@ export default function Data() {
           <h4> The Web Scraper is made with Scrapy, xpath, and the os module in Python. I am scraping data from the NASA government website as physics is one of my special interests.
                 By searching for data rows that have the content needed, find data to save to SQL. </h4>
           
-    <hr>
+    <hr />
           <h3> SQL database </h3>
           <a href = "https://github.com/TREYCSE/Data_SQL" className="title"> Database in progress </a>
           <h4> In progress... </h4>
