58201b4d665087d420c3c126f4e5031372b1afed
The Ethical Mad Scientist <117105883+TREYCSE@users.noreply.github.com>
2023-10-16 09:57:58 -0400

    Update data.js

--- website/src/components/data.js
+++ website/src/components/data.js
@@ -4,12 +4,19 @@ export default function Data() {
           <h3> WEB SCRAPER </h3>
           <a href = "https://github.com/TREYCSE/Web_Scraper_csv" className="title">  Web scraping tool in progress </a>
           <h4> The Web Scraper is made with Scrapy, xpath, and the os module in Python. I am scraping Legal data from tgovernment websites such as pulling articles from the Constitution full text online. Now that the data has been parsed, I am converting to SQL to store in files. </h4>
-          
-          <h3> SQL database </h3>
-          <a href = "https://github.com/TREYCSE/Data_SQL" className="title"> Database in progress </a>
-          <h4> In progress... </h4>
+          <h6> While in progress, considering switching from xpath for this form of data and instead using css. Stay tuned. </h6>
       </div>
     )
   }
+
+
+
+/*
+
+          <h3> SQL database </h3>
+          <a href = "https://github.com/TREYCSE/Data_SQL" className="title"> Database in progress </a>
+          <h4> In progress... </h4>
+
+*/
 
 
