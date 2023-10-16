24400c4417fd9370597aa6ec913a2d058a8914a6
The Ethical Mad Scientist <117105883+TREYCSE@users.noreply.github.com>
2023-10-16 09:55:22 -0400

    Update data.js

--- website/src/components/data.js
+++ website/src/components/data.js
@@ -3,8 +3,7 @@ export default function Data() {
     <div className="">
           <h3> WEB SCRAPER </h3>
           <a href = "https://github.com/TREYCSE/Web_Scraper_csv" className="title">  Web scraping tool in progress </a>
-          <h4> The Web Scraper is made with Scrapy, xpath, and the os module in Python. I am scraping data from the NASA government website as physics is one of my special interests.
-                By searching for data rows that have the content needed, find data to save to SQL. </h4>
+          <h4> The Web Scraper is made with Scrapy, xpath, and the os module in Python. I am scraping Legal data from tgovernment websites such as pulling articles from the Constitution full text online. Now that the data has been parsed, I am converting to SQL to store in files. </h4>
           
           <h3> SQL database </h3>
           <a href = "https://github.com/TREYCSE/Data_SQL" className="title"> Database in progress </a>
