5edae5f036d33e898ce9c18b007850f8454c7565
The Ethical Mad Scientist <117105883+TREYCSE@users.noreply.github.com>
2023-11-06 17:29:02 -0500

    Update data.js

--- website/src/components/data.js
+++ website/src/components/data.js
@@ -3,7 +3,8 @@ export default function Data() {
     <div className="">
           <h3> WEB SCRAPER </h3>
           <a href = "https://github.com/TREYCSE/Web_Scraper_csv" className="title">  Web scraping tool in progress </a>
-          <h4> The Web Scraper is made with Scrapy, xpath, and the os module in Python. I am scraping Legal data from government websites such as pulling articles from the Constitution full text online.
+          <h4> The Web Scraper is made with Scrapy in Python and the data is saved as json files.
+                I am scraping Legal data from government websites such as pulling articles from the Constitution full text online.
                 Now that the data has been parsed, I am focusing on deploying and automation with Azure PostgreSQL and GitHub Actions.
                 In addition, looking for websites with surplus of data, rather than limited articles. In research phase continually.</h4>
           <h6> Considering other legal data to scrape that is better formatted and therefore better to parse through. </h6>
