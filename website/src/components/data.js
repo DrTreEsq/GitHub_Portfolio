f0b9e6052e0834313e1e007e6bf8a34be5fdd164
The Ethical Mad Scientist <117105883+TREYCSE@users.noreply.github.com>
2023-11-06 17:32:19 -0500

    Update data.js

--- website/src/components/data.js
+++ website/src/components/data.js
@@ -8,7 +8,7 @@ export default function Data() {
                 Now that the data has been parsed, I am focusing on deploying and automation with Azure PostgreSQL and GitHub Actions.
                 In addition, looking for websites with surplus of data, rather than limited articles. In research phase continually.</h4>
           <h6 className="black"> Considering other legal data to scrape that is better formatted and therefore better to parse through. </h6>
-          <h6 className="black"> SQL medical database also in progress, researching best sites to scrape - stay tuned on progress </h6>
+          <h6 className="black"> SQL medical database also in progress, researching best sites to scrape and clean - stay tuned on progress! </h6>
       </div>
     )
   }
