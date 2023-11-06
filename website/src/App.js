d4391b09a769890df08fc0f57a964e2a5c818703
The Ethical Mad Scientist <117105883+TREYCSE@users.noreply.github.com>
2023-11-06 17:21:02 -0500

    Update App.js

--- website/src/App.js
+++ website/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";  //useState, useEffect } 
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Nav, Home, Aboutme, Algorithms, Codes, Consulting, Contact, Data, Education, Skills, Services, Tutorials, YoutubeEmbed, YoutubeEmbed2, YoutubeEmbed3} from './components/index.js'
 import './App.css';
+
+//use HashRouter to fix manual reloading?
 
 function App() {
 
@@ -22,7 +24,7 @@ function App() {
       <Nav Home="/" Aboutme="/aboutme" Algorithms="/algorithms" Codes="/codes" Consulting="/consulting" Contact="/contact" Data="/data" Education="/education" Skills="/skills" Services="/services"
       Tutorials="/tutorials" YoutubeEmbed="/hardware" YoutubeEmbed2="neural_networks" YoutubeEmbed3="/production"/>
 
-      <HashRouter>
+      <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/aboutme" element={<Aboutme />}/>
         <Route path="/algorithms" element={<Algorithms />}/> 
@@ -37,7 +39,7 @@ function App() {
         <Route path="/hardware" element={<YoutubeEmbed embedId="Utwnm2kjYAM" embedId3="4SA6t_tcFQA"/>} />
         <Route path="/neural_networks" element={<YoutubeEmbed2 embedId2="_c_x8A3mNDk"/>} />
         <Route path="/production" element={<YoutubeEmbed3 embedId="NgWujOrCZFo"/>} />
-      </HashRouter>
+      </Routes>
 
 
     </div>
