e3b46702928ab5f9e0c897348183d45344f0c328
The Ethical Mad Scientist <117105883+TREYCSE@users.noreply.github.com>
2023-11-06 17:32:07 -0500

    Update data.js

--- website/src/components/data.js
+++ website/src/components/data.js
@@ -1,14 +1,14 @@
 export default function Data() {
   return (
     <div className="">
-          <h3> WEB SCRAPER </h3>
+          <h3 className="black"> WEB SCRAPER </h3>
           <a href = "https://github.com/TREYCSE/Web_Scraper_csv" className="title">  Web scraping tool in progress </a>
-          <h4> The Web Scraper is made with Scrapy in Python and the data is saved as json files.
+          <h4 className="black"> The Web Scraper is made with Scrapy in Python and the data is saved as json files.
                 I am scraping Legal data from government websites such as pulling articles from the Constitution full text online.
                 Now that the data has been parsed, I am focusing on deploying and automation with Azure PostgreSQL and GitHub Actions.
                 In addition, looking for websites with surplus of data, rather than limited articles. In research phase continually.</h4>
-          <h6> Considering other legal data to scrape that is better formatted and therefore better to parse through. </h6>
-          <h6> SQL legal and medical database also in progress, searching for best sites to scrape - stay tuned on progress </h6>
+          <h6 className="black"> Considering other legal data to scrape that is better formatted and therefore better to parse through. </h6>
+          <h6 className="black"> SQL medical database also in progress, researching best sites to scrape - stay tuned on progress </h6>
       </div>
     )
   }
