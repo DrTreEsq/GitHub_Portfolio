944d2ac1ffb7b21b059ffc90edc5717c46e2ea5a
The Ethical Mad Scientist <117105883+TREYCSE@users.noreply.github.com>
2024-05-23 10:59:45 -0400

    Update about_me.js

--- website/src/components/about_me.js
+++ website/src/components/about_me.js
@@ -4,7 +4,7 @@ export default function Aboutme() {
   return (
       <div>
         <hr></hr>
-        <h4> On track to be Educator (currently teaching), Attorney (in law school), Biohacker (combining knowledge of human body and computer science to advance human medicine over the next decade or so), Biomedical Engineer (bringing something to market soon - stay tuned), Doctor (med school starts after company launch), Neurocientist (dissertation in progress, starting degree with med school) </h4>
+        <h4 className="black"> On track to be Educator (currently teaching), Attorney (in law school), Biohacker (combining knowledge of human body and computer science to advance human medicine over the next decade or so), Biomedical Engineer (bringing something to market soon - stay tuned), Doctor (med school starts after company launch), Neuroscientist (dissertation in progress, starting degree with med school) </h4>
         <h1 className="title">The Ethical Mad Scientist; Research & Computer Scientist </h1>
         <Navs Education="/education" Skills="/skills" Projects="/projects"/>
         <h6 className="black">Welcome, Earthling! My name is Tre and I am a Lifelong Learner with a goal of learning each valuable skill necessary to pioneer the change that I wish to see in the world.
