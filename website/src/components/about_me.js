c7113ac7634315717eb29229f1ca475bc0cd19e5
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-06-23 12:37:57 -0400

    Update about_me.js

--- website/src/components/about_me.js
+++ website/src/components/about_me.js
@@ -6,10 +6,9 @@ export default function Aboutme() {
         <hr></hr>
         <Navs Education="/education" Skills="/skills" Projects="/projects"/>
         <h4 className="black"> On track to be Educator (currently teaching), Attorney (in law school), Biohacker (combining knowledge of human body and computer science to advance human medicine over the next decade or so), Biomedical Engineer (bringing something to market soon - stay tuned), Doctor (med school starts after company launch), Neuroscientist (dissertation in progress, starting degree with med school) </h4>
-        <h3 className="black">Welcome, Earthling! My name is Tre and I am an Educator and Lifelong Learner with a goal of learning each valuable skill necessary to pioneer the change that I wish to see in the world.
-          I have a wide range of abilities stemming from my diverse work background, education, and additional academic interests ranging from neuroscience, programming, to the history of mankind and more.
-          I hope to continually gain experience to grow myself professionally and offer the same to the organizations that I work with as I work towards a greater goal of personal development and global change.
-          </h3>
+        <h3 className="black"> Welcome, Earthling! My name is Tre and I am an Educcator, Lifelong Learner, and Interdisciplinery Researcher with a goal of learning each valuable skill necessary to pioneer the change that I wish to see in the world.
+                                I hope to continually gain experience to grow myself professionally and offer the same to the organizations that I work with as I work towards a greater goal of personal development and global change.
+        </h3>
         <img className="example" src="https://i.postimg.cc/xjvnSwnq/mx05abasr3s01.jpg" align="center" alt="treytheurkel"></img>
         
       </div>
