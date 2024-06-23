0b6fea11a12edc46190d82c16052d28c89d237fc
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-06-23 12:43:55 -0400

    Update contact.js

--- website/src/components/contact.js
+++ website/src/components/contact.js
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
         <div className="contact">
-          <a className="msglink" href="https://www.linkedin.com/in/teachertrey/"> LinkedIn </a>
+          <a className="msglink" href="https://www.linkedin.com/in/wrongaddress/"> LinkedIn </a>
           <a className="msglink" href="https://docs.google.com/forms/d/e/1FAIpQLSfx8rrHRvZVKwNzhOCqloYEAXB0cLLrBxR9DBzJG3VFVsp2Fg/viewform?usp=sf_link"> Send me a message! </a>
 
         </div>
