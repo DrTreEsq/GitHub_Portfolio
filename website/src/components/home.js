e5b4a2ba35180e1b4983c5c82d5e939b925901f4
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-06-26 10:19:32 -0400

    Update home.js

--- website/src/components/home.js
+++ website/src/components/home.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
       <div>
-        <h2> Welcome to the Website Portfolio for Research Scientist</h2>
-        <h1 className="title white"> Tre Weathers</h1>
+        <h2> Welcome to the Website Portfolio for Interdisciplinary Researcher </h2>
+        <h1 className="title white"> Tre Joy</h1>
         <img className="meimg" src="https://i.postimg.cc/9QnRRJJ3/IMG-6355.jpg" alt="that's me right there!"></img>
       
       </div>
