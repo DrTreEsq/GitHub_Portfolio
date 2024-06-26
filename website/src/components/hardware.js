a3ca82164c01695fdc61b1ca809262e6465d97b7
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-06-26 09:34:04 -0400

    Update hardware.js

--- website/src/components/hardware.js
+++ website/src/components/hardware.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const YoutubeEmbed = ({embedId, embedId3}) => (
+const YoutubeEmbed = ({embedId}) => (
   <div>
     <iframe
       className="video-big"
