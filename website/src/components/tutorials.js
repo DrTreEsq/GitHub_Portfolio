320c58def90877e186d190c33bfe724899f36e99
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-06-26 10:04:56 -0400

    Update tutorials.js

--- website/src/components/tutorials.js
+++ website/src/components/tutorials.js
@@ -4,8 +4,8 @@ export default function Tutorials() {
   return (
       <div>
 
-        <p>While stalking me casually perusing my portfolio, maybe you'll stumble upon something that you're interested in trying out.
-          Here, you will find a few walk-throughs for some of the nifty things i've come across to do in order to gain my desired skill set.</p>
+        <h3>While stalking me casually perusing my portfolio, maybe you'll stumble upon something that you're interested in trying out.
+          Here, you will find a few walk-throughs for some of the nifty things i've come across to do in order to gain my desired skill set.</h3>
 
         <Nave YoutubeEmbed="/hardware"  YoutubeEmbed3="/production" YoutubeEmbed2="/neural_networks"/>
         
