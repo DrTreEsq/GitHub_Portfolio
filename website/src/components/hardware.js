09331aeb548a7c6aa43d039622d9bca467d1ab4c
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-06-26 08:58:15 -0400

    Update hardware.js

--- website/src/components/hardware.js
+++ website/src/components/hardware.js
@@ -4,17 +4,6 @@ import PropTypes from "prop-types";
 const YoutubeEmbed = ({ embedId, embedId3}) => (
   <div>
 
-  <iframe 
-      className="video-big"
-      width="853"
-      height="480"
-      src={`https://www.youtube.com/embed/${embedId3}`}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Embedded youtube"
-    />
-
     <iframe
       className="video-big"
       width="853"
