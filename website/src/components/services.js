1d170c5625555c973d9d85eb8be42b84bd8ba0a0
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-06-26 10:18:36 -0400

    Update services.js

--- website/src/components/services.js
+++ website/src/components/services.js
@@ -4,7 +4,7 @@ export default function Services() {
   return (
       <div className="">
         
-        <h5> click for code samples, results, summaries, and visuals from my completed and in progress projects! </h5>
+        <h3> click buttons below for code samples, results, summaries, visuals, and more from my completed and in-progress projects! </h3>
         <Navd Data="/data" Algorithms="/algorithms"  Codes="/codes" Consulting="/consulting" />
         <hr></hr>
         <a className="github" href="https://github.com/TREYCSE/GitHub_Portfolio">GitHub PortFolio README.md for Engineers: Click here for code samples, etc.</a>
