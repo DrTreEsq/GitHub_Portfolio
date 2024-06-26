6b0d4263bdceecf51c167aee9992cbfbef18f20e
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-06-26 09:37:48 -0400

    Update contact.js

--- website/src/components/contact.js
+++ website/src/components/contact.js
@@ -3,7 +3,7 @@ export default function Contact() {
         <div className="contact">
           <a className="msglink" href="https://www.linkedin.com/in/wrongaddress/"> LinkedIn </a>
           <a className="msglink" href="https://docs.google.com/forms/d/e/1FAIpQLSfx8rrHRvZVKwNzhOCqloYEAXB0cLLrBxR9DBzJG3VFVsp2Fg/viewform?usp=sf_link"> Send me a message! </a>
-          <h4> techteachertrey@gmail.com, CEO@theemstech.com, 347-228-1883 </h4>
+          <h2 className="black"> techteachertrey@gmail.com, CEO@theemstech.com, 347-228-1883 </h2>
         </div>
       )
     }
