ef448179d865309531b24b4156db317b6b01f5fc
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-07-27 19:46:50 -0400

    Update nav.js

--- website/src/components/nav.js
+++ website/src/components/nav.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 
-export default function Nav({Home, Aboutme , Contact, Services, Tutorials}) {
+export default function Nav({Home, Aboutme , Services, Projects, Contact, Tutorials}) {
   return (
     <div>
       <nav className="homenav">
           <Link className="link white" to={Home}> Home </Link>
           <Link className="link white" to={Aboutme}> About Me </Link>
           <Link className="link white" to={Services}> Services </Link>
+          <Link className="link white" to={Projects}> Projects </Link>
           <Link className="link white" to={Contact}> Contact </Link>
           <Link className="link white" to={Tutorials}> Tutorials </Link>
       </nav>
