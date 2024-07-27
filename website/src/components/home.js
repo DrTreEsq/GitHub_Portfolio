dfcfdcd573e6c04fab474ce9e90f65f0df7ac902
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-07-27 15:22:16 -0400

    Update home.js

--- website/src/components/home.js
+++ website/src/components/home.js
@@ -4,7 +4,7 @@ export default function Home() {
         <h2> Welcome to the Website Portfolio for Interdisciplinary Researcher </h2>
         <h1 className="title white"> Tre Joy</h1>
         <img className="meimg" src="https://i.postimg.cc/9QnRRJJ3/IMG-6355.jpg" alt="that's me right there!"></img>
-      
+        <img className="robot" src="https://i.postimg.cc/kXMzVJtC/humanoid-robot-military-robot-artificial-intelligence-robotics-thumbnail.jpg" alt="robot"> </img>
       </div>
     )
   }
