b6dc502e419399bfb6484502a4457f6f128942ad
trey_weathers <treychelle.weathers2017@gmail.com>
2024-07-28 09:58:06 -0400

    update about_me

--- website/src/components/about_me.js
+++ website/src/components/about_me.js
@@ -11,13 +11,9 @@ export default function Aboutme() {
         
         <h3 className="black"> Welcome, Earthling! My name is Tre and I am an Educator, Lifelong Learner, and Interdisciplinery Researcher with a goal of learning each valuable skill necessary to pioneer the change that I wish to see in the world.
         I hope to continually gain experience to grow myself professionally and offer the same to the organizations that I work with as I work towards a greater goal of personal development and global change.
-        </h3>
-  
-        <img className="example" src="https://i.postimg.cc/xjvnSwnq/mx05abasr3s01.jpg" align="center" alt="treytheurkel"></img>
-
-        <h4 className="black"> On track to be Tenured Professor (currently teaching k-12), Attorney (in law school), Biohacker (combining knowledge of human body and computer science to advance human medicine over the next decade or so),
+        On track to be Tenured Professor (currently teaching k-12), Attorney (in law school), Biohacker (combining knowledge of human body and computer science to advance human medicine over the next decade or so),
         Biomedical Engineer (bringing something to market soon - stay tuned), Clinical Geneticist/Doctor (med school starts after company launch), Computational Neuroscientist (taking online courses, dissertation in progress, starting degree with med school),
-        and that's enough about me! Reach out and tell me about yourself🤩! </h4>
+        and that's enough about me! Reach out and tell me about yourself🤩! </h3>
         </p>
     
       </div>
