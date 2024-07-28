549da36eb9350cb85a24f42011defced0338df87
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-07-27 20:32:04 -0400

    Update home.js

--- website/src/components/home.js
+++ website/src/components/home.js
@@ -4,8 +4,9 @@ export default function Home() {
         <h2> Welcome to the Website Portfolio for Interdisciplinary Researcher </h2>
         <h1 className="title white"> Tre Joy</h1>
         <h3> Click the images below for the inside scoop! </h3>
+        <a href="https://github.com/DrTreEsq?tab=repositories"> <img className="robot" src="https://i.postimg.cc/gjqVhnwr/d3871fb68bb5c11b3585aa3e61d6b7b3.jpg" alt="robot"></img> </a>
         <a href="projects_2.js"> <img className="robot" src="https://i.postimg.cc/kGmTbr5M/IMG-0551.jpg" alt="robot"></img> </a>
-        <a href="https://github.com/DrTreEsq?tab=repositories"> <img className="robot" src="https://i.postimg.cc/ydJFrB1P/IMG-1486.avif" alt="robot"></img> </a>
+       c<a href="https://github.com/DrTreEsq?tab=repositories"> <img className="robot" src="https://i.postimg.cc/ydJFrB1P/IMG-1486.avif" alt="robot"></img> </a>
       </div>
     )
   }
