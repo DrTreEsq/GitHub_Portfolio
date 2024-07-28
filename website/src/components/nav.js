1394ba0ea264e80180f2d2eff8151049c46aef3f
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-07-28 08:30:26 -0400

    Update nav.js

--- website/src/components/nav.js
+++ website/src/components/nav.js
@@ -4,12 +4,12 @@ export default function Nav({Home, Aboutme , Services, Projects, Contact, Tutori
   return (
     <div>
       <nav className="homenav">
-          <Link className="link white" to={Home}> Home <img  src="https://i.postimg.cc/kGmTbr5M/IMG-0551.jpg" alt="robot"></img> </Link>
-          <Link className="link white" to={Aboutme}> About Me <img  src="https://i.postimg.cc/ydJFrB1P/IMG-1486.avif" alt="robot"></img> </Link>
-          <Link className="link white" to={Services}> Services <img  src="https://i.postimg.cc/gjqVhnwr/d3871fb68bb5c11b3585aa3e61d6b7b3.jpg" alt="robot"></img> </Link>
-          <Link className="link white" to={Projects}> Projects <img  src="https://i.postimg.cc/kGmTbr5M/IMG-0551.jpg" alt="robot"></img> </Link>
-          <Link className="link white" to={Contact}> Contact <img  src="https://i.postimg.cc/ydJFrB1P/IMG-1486.avif" alt="robot"></img> </Link>
-          <Link className="link white" to={Tutorials}> Tutorials <img  src="https://i.postimg.cc/kGmTbr5M/IMG-0551.jpg" alt="robot"></img> </Link>
+          <Link className="link white" to={Home}> Home </Link>
+          <Link className="link white" to={Aboutme}> About Me  </Link>
+          <Link className="link white" to={Services}> Services </Link>
+          <Link className="link white" to={Projects}> Projects  </Link>
+          <Link className="link white" to={Contact}> Contact  </Link>
+          <Link className="link white" to={Tutorials}> Tutorials  </Link>
       </nav>
     </div>
   );
