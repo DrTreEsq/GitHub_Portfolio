7a317220aef90a5ba6d51afdc455419ba40ffe0e
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-07-27 22:02:08 -0400

    Update nav.js

--- website/src/components/nav.js
+++ website/src/components/nav.js
@@ -5,8 +5,8 @@ export default function Nav({Home, Aboutme , Services, Projects, Contact, Tutori
     <div>
       <nav className="homenav">
           <Link className="link white" to={Home}> Home <img className="navrobot" src="https://i.postimg.cc/kGmTbr5M/IMG-0551.jpg" alt="robot"></img> </Link>
-          <Link className="link white" to={Aboutme}> About Me </Link>
-          <Link className="link white" to={Services}> Services </Link>
+          <Link className="link white" to={Aboutme}> About Me <img className="navrobot" src="https://i.postimg.cc/ydJFrB1P/IMG-1486.avif" alt="robot"></img </Link>
+          <Link className="link white" to={Services}> Services <img className="navrobot" src="https://i.postimg.cc/gjqVhnwr/d3871fb68bb5c11b3585aa3e61d6b7b3.jpg" alt="robot"></img> </Link>
           <Link className="link white" to={Projects}> Projects </Link>
           <Link className="link white" to={Contact}> Contact </Link>
           <Link className="link white" to={Tutorials}> Tutorials </Link>
@@ -14,8 +14,3 @@ export default function Nav({Home, Aboutme , Services, Projects, Contact, Tutori
     </div>
   );
 }
-
-/*
-        <a href="https://github.com/DrTreEsq?tab=repositories"> <img className="robot" src="https://i.postimg.cc/ydJFrB1P/IMG-1486.avif" alt="robot"></img> </a>
-        <a href="https://github.com/DrTreEsq?tab=repositories"> <img className="robot" src="https://i.postimg.cc/gjqVhnwr/d3871fb68bb5c11b3585aa3e61d6b7b3.jpg" alt="robot"></img> </a>
-*/
