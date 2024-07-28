68883be5b88ebbbf64ffcce217a83088421b9e68
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-07-27 22:37:54 -0400

    Update nav.js

--- website/src/components/nav.js
+++ website/src/components/nav.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Nav({Home, Aboutme , Services, Projects, Contact, Tutorials}) {
   return (
     <div>
-      <nav>
+      <nav className="homenav">
           <Link className="link white" to={Home}> Home <img  src="https://i.postimg.cc/kGmTbr5M/IMG-0551.jpg" alt="robot"></img> </Link>
           <Link className="link white" to={Aboutme}> About Me <img  src="https://i.postimg.cc/ydJFrB1P/IMG-1486.avif" alt="robot"></img> </Link>
           <Link className="link white" to={Services}> Services <img  src="https://i.postimg.cc/gjqVhnwr/d3871fb68bb5c11b3585aa3e61d6b7b3.jpg" alt="robot"></img> </Link>
@@ -16,7 +16,6 @@ export default function Nav({Home, Aboutme , Services, Projects, Contact, Tutori
 }
 
 /*
-className="homenav"
           <Link className="link white" to={Home}> Home <img className="navrobot" src="https://i.postimg.cc/kGmTbr5M/IMG-0551.jpg" alt="robot"></img> </Link>
           <Link className="link white" to={Aboutme}> About Me <img className="navrobot" src="https://i.postimg.cc/ydJFrB1P/IMG-1486.avif" alt="robot"></img> </Link>
           <Link className="link white" to={Services}> Services <img className="navrobot" src="https://i.postimg.cc/gjqVhnwr/d3871fb68bb5c11b3585aa3e61d6b7b3.jpg" alt="robot"></img> </Link>
