9eed4a008c46abef82ed24d3f4b303bf73955894
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-07-28 08:31:14 -0400

    Update home.js

--- website/src/components/home.js
+++ website/src/components/home.js
@@ -6,7 +6,7 @@ export default function Home() {
         <h3> Click the images below for the inside scoop! </h3>
         <a href="https://brave-glacier-00e751b0f.3.azurestaticapps.net/projects"> <img className="robot" src="https://i.postimg.cc/kGmTbr5M/IMG-0551.jpg" alt="robot"></img> </a>
         <a href="https://github.com/DrTreEsq?tab=repositories"> <img className="robot" src="https://i.postimg.cc/ydJFrB1P/IMG-1486.avif" alt="robot"></img> </a>
-        <a href="https://github.com/DrTreEsq?tab=repositories"> <img className="robot" src="https://i.postimg.cc/gjqVhnwr/d3871fb68bb5c11b3585aa3e61d6b7b3.jpg" alt="robot"></img> </a>
+        <a href="https://brave-glacier-00e751b0f.3.azurestaticapps.net/services"> <img className="robot" src="https://i.postimg.cc/gjqVhnwr/d3871fb68bb5c11b3585aa3e61d6b7b3.jpg" alt="robot"></img> </a>
       </div>
     )
   }
