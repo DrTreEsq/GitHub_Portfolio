2e3ef847b408a0c047a26cb7b11e580423ff56df
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-07-27 20:47:20 -0400

    Update nav.js

--- website/src/components/nav.js
+++ website/src/components/nav.js
@@ -4,7 +4,7 @@ export default function Nav({Home, Aboutme , Services, Projects, Contact, Tutori
   return (
     <div>
       <nav className="homenav">
-          <Link className="link white" to={Home}> Home </Link>
+          <Link className="link white" to={Home}> Home <img className="robot" src="https://i.postimg.cc/kGmTbr5M/IMG-0551.jpg" alt="robot"></img> </Link>
           <Link className="link white" to={Aboutme}> About Me </Link>
           <Link className="link white" to={Services}> Services </Link>
           <Link className="link white" to={Projects}> Projects </Link>
@@ -14,3 +14,8 @@ export default function Nav({Home, Aboutme , Services, Projects, Contact, Tutori
     </div>
   );
 }
+
+/*
+        <a href="https://github.com/DrTreEsq?tab=repositories"> <img className="robot" src="https://i.postimg.cc/ydJFrB1P/IMG-1486.avif" alt="robot"></img> </a>
+        <a href="https://github.com/DrTreEsq?tab=repositories"> <img className="robot" src="https://i.postimg.cc/gjqVhnwr/d3871fb68bb5c11b3585aa3e61d6b7b3.jpg" alt="robot"></img> </a>
+*/
