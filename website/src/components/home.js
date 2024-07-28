5ad0c41638ef3220b5f4da34c08bbce7b46825a8
trey_weathers <treychelle.weathers2017@gmail.com>
2024-07-28 09:32:03 -0400

    animation cleanup and nav edits

--- website/src/components/home.js
+++ website/src/components/home.js
@@ -3,10 +3,13 @@ export default function Home() {
       <div>
         <h2> Welcome to the Website Portfolio for Interdisciplinary Researcher </h2>
         <h1 className="title white"> Tre Joy</h1>
+      </div>
+    )
+  }
+
+  /*
         <h3> Click the images below for the inside scoop! </h3>
         <a href="https://brave-glacier-00e751b0f.3.azurestaticapps.net/projects"> <img className="robot" src="https://i.postimg.cc/kGmTbr5M/IMG-0551.jpg" alt="robot"></img> </a>
         <a href="https://github.com/DrTreEsq?tab=repositories"> <img className="robot" src="https://i.postimg.cc/ydJFrB1P/IMG-1486.avif" alt="robot"></img> </a>
         <a href="https://brave-glacier-00e751b0f.3.azurestaticapps.net/services"> <img className="robot" src="https://i.postimg.cc/gjqVhnwr/d3871fb68bb5c11b3585aa3e61d6b7b3.jpg" alt="robot"></img> </a>
-      </div>
-    )
-  }
+  */
