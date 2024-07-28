4517dd2617c132f0f4039f29cef0adcba0e4fa5b
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-07-27 22:15:50 -0400

    Update nav.js

--- website/src/components/nav.js
+++ website/src/components/nav.js
@@ -7,9 +7,9 @@ export default function Nav({Home, Aboutme , Services, Projects, Contact, Tutori
           <Link className="link white" to={Home}> Home <img className="navrobot" src="https://i.postimg.cc/kGmTbr5M/IMG-0551.jpg" alt="robot"></img> </Link>
           <Link className="link white" to={Aboutme}> About Me <img className="navrobot" src="https://i.postimg.cc/ydJFrB1P/IMG-1486.avif" alt="robot"></img> </Link>
           <Link className="link white" to={Services}> Services <img className="navrobot" src="https://i.postimg.cc/gjqVhnwr/d3871fb68bb5c11b3585aa3e61d6b7b3.jpg" alt="robot"></img> </Link>
-          <Link className="link white" to={Projects}> Projects </Link>
-          <Link className="link white" to={Contact}> Contact </Link>
-          <Link className="link white" to={Tutorials}> Tutorials </Link>
+          <Link className="link white" to={Projects}> Projects <img className="navrobot" src="https://i.postimg.cc/kGmTbr5M/IMG-0551.jpg" alt="robot"></img> </Link>
+          <Link className="link white" to={Contact}> Contact <img className="navrobot" src="https://i.postimg.cc/ydJFrB1P/IMG-1486.avif" alt="robot"></img> </Link>
+          <Link className="link white" to={Tutorials}> Tutorials <img className="navrobot" src="https://i.postimg.cc/kGmTbr5M/IMG-0551.jpg" alt="robot"></img> </Link>
       </nav>
     </div>
   );
