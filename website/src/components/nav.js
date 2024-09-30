6723a431a4668cdb0ee261fb87ee348f0210dfdd
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-09-30 19:16:03 -0400

    Update nav.js

--- website/src/components/nav.js
+++ website/src/components/nav.js
@@ -5,7 +5,7 @@ export default function Nav({Home, Aboutme , Services, Projects, Contact, Tutori
     <div>
       <nav className="homenav">
           <h2> Welcome to the Website Portfolio for Interdisciplinary Researcher </h2>
-          <h1 className="title white"> Tre Joy</h1>
+          <h1 className="title white"> Tre Weathers </h1>
           <Link className="link white" to={Home}> Home <img className="navrobot" src="https://i.postimg.cc/kGmTbr5M/IMG-0551.jpg" alt="robot"></img> </Link>
           <Link className="link white" to={Aboutme}> About Me <img className="navrobot" src="https://i.postimg.cc/ydJFrB1P/IMG-1486.avif" alt="robot"></img> </Link>
           <Link className="link white" to={Services}> Services <img className="navrobot" src="https://i.postimg.cc/gjqVhnwr/d3871fb68bb5c11b3585aa3e61d6b7b3.jpg" alt="robot"></img> </Link>
