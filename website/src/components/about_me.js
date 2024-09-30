7d0e9ddcd6109f7696b1d024f78a543aa9c4f584
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2024-09-30 19:05:39 -0400

    Update about_me.js

--- website/src/components/about_me.js
+++ website/src/components/about_me.js
@@ -13,10 +13,9 @@ export default function Aboutme() {
         <h3 className="black"> Welcome, Earthling! My name is Tre and I am an Educator, Lifelong Learner, and Interdisciplinary Researcher with a goal of learning each valuable skill necessary to pioneer the change that I wish to see in the world.
         I hope to continually gain experience to grow myself professionally and offer the same to the organizations that I work with as I work towards a greater goal of personal development and global change.
         On track to be Tenured Professor (currently teaching k-12), Attorney (in law school), Biohacker (combining knowledge of human body and computer science to advance human medicine over the next decade or so),
-        Biomedical Engineer (bringing something to market soon - stay tuned), Clinical Geneticist/Doctor (med school starts after company launch), Computational Neuroscientist (taking online courses, dissertation in progress, starting degree with med school),
-        and that's enough about me! Reach out and tell me about yourself🤩! </h3>
+        Biomedical Engineer (bringing something to market soon - stay tuned), Computational Neuroscientist (taking online courses, dissertation in progress), and that's enough about me! Reach out and tell me about yourself🤩! </h3>
         </p>
-    
+
       </div>
     )
   }
