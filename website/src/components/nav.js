de0408e779130b812f0691769642ed9493e0e09f
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2025-05-07 13:40:10 -0400

    Update nav.js

--- website/src/components/nav.js
+++ website/src/components/nav.js
@@ -10,7 +10,7 @@ export default function Nav({Home, Aboutme , Services, Projects, Contact, Tutori
           <Link className="link white" to={Aboutme}> About Me </Link>
           <Link className="link white" to={Services}> Services </Link>
           <Link className="link white" to={Projects}> Projects </Link>
-          <Link className="link white" to={Contact}> Contact  </Link>
+          <Link className="link white" to={Contact}> Contact </Link>
           <Link className="link white" to={Tutorials}> Tutorials </Link>
       </nav>
     </div>
