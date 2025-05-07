be8ebe8aadce2783c4d60af75bcafe765b66458c
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2025-05-07 13:34:25 -0400

    Update nav.js

--- website/src/components/nav.js
+++ website/src/components/nav.js
@@ -6,6 +6,27 @@ export default function Nav({Home, Aboutme , Services, Projects, Contact, Tutori
       <nav className="homenav">
           <h2> Welcome to the Website Portfolio for Interdisciplinary Researcher </h2>
           <h1 className="title white"> Tre Weathers </h1>
+          <Link className="link white" to={Home}> Homec </Link>
+          <Link className="link white" to={Aboutme}> </Link>
+          <Link className="link white" to={Services}> Services </Link>
+          <Link className="link white" to={Projects}> Projects </Link>
+          <Link className="link white" to={Contact}> Contact  </Link>
+          <Link className="link white" to={Tutorials}> Tutorials </Link>
+      </nav>
+    </div>
+  );
+}
+
+
+/*
+import { Link } from "react-router-dom";
+
+export default function Nav({Home, Aboutme , Services, Projects, Contact, Tutorials}) {
+  return (
+    <div>
+      <nav className="homenav">
+          <h2> Welcome to the Website Portfolio for Interdisciplinary Researcher </h2>
+          <h1 className="title white"> Tre Weathers </h1>
           <Link className="link white" to={Home}> Home <img className="navrobot" src="https://i.postimg.cc/kGmTbr5M/IMG-0551.jpg" alt="robot"></img> </Link>
           <Link className="link white" to={Aboutme}> About Me <img className="navrobot" src="https://i.postimg.cc/ydJFrB1P/IMG-1486.avif" alt="robot"></img> </Link>
           <Link className="link white" to={Services}> Services <img className="navrobot" src="https://i.postimg.cc/gjqVhnwr/d3871fb68bb5c11b3585aa3e61d6b7b3.jpg" alt="robot"></img> </Link>
@@ -16,3 +37,5 @@ export default function Nav({Home, Aboutme , Services, Projects, Contact, Tutori
     </div>
   );
 }
+
+*/
