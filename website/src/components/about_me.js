1176ecb56ed657602c04a4e893d27928789c9163
CEO of T.E.M.S Tech <117105883+DrTreEsq@users.noreply.github.com>
2025-05-16 11:34:48 -0400

    Update about_me.js

--- website/src/components/about_me.js
+++ website/src/components/about_me.js
@@ -10,12 +10,14 @@ export default function Aboutme() {
         <h2> Lifelong Learner📚, Advocate🧑‍⚖️, Biohacker🧬, Researcher🥼, Artist🎭, and Technologist🧑‍💻 (LAB RAT) </h2>
         <img className="meimg" src="https://i.postimg.cc/9QnRRJJ3/IMG-6355.jpg" alt="that's me right there!"></img>
         
-        <h3 className="black"> Welcome, Earthling! My name is Tre and I am an Educator, Lifelong Learner, and Interdisciplinary Researcher with a goal of learning each valuable skill necessary to pioneer the change that I wish to see in the world.
-        I hope to continually gain experience to grow myself professionally and offer the same to the organizations that I work with as I work towards a greater goal of personal development and global change.
-        On track to be Tenured Professor (currently teaching k-12), Attorney (in law school), Biohacker (combining knowledge of human body and computer science to advance human medicine over the next decade or so),
-        Biomedical Engineer (bringing something to market soon - stay tuned), Computational Neuroscientist (taking online courses, dissertation in progress), and that's enough about me! Reach out and tell me about yourself🤩! </h3>
-        </p>
-
+        <h3 className="black"> Welcome, Earthling! My name is Tre and I am an Educator, Lifelong Learner, and Interdisciplinary Researcher with a goal of learning each valuable skill necessary to pioneer the change that I wish to see in the world! </h3>
+        <p> I hope to continually gain experience to grow myself professionally and offer the same to the schools and organizations that I work with as I work towards a greater goal of personal development and global change. </p>
+    
       </div>
     )
   }
+
+/*
+        On track to be Tenured Professor (currently teaching k-12), Attorney (in law school), Biohacker (combining knowledge of human body and computer science to advance human medicine over the next decade or so),
+        Biomedical Engineer (bringing something to market soon - stay tuned), Computational Neuroscientist (taking online courses, dissertation in progress), and that's enough about me! Reach out and tell me about yourself🤩
+*/
